bca59bb92a745b78b9105e5117fc9c8d517dfdef
AMON NATHAN BOAZ AKPABLIN <amonnathanboazakpablin@MacBook-Pro-de-AMON.local>
2025-11-28 09:40:43 +0100

    sol bug nbr item dans le panier

--- client/src/AppHeader.vue
+++ client/src/AppHeader.vue
@@ -64,7 +64,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
             </svg>
             <span class="absolute -top-1 -right-1 bg-neutral-900 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center font-bold shadow-lg">
-              0
+              {{ cartStore.itemCount }}
             </span>
             <span class="absolute -bottom-8 left-1/2 -translate-x-1/2 bg-neutral-900 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap pointer-events-none">
               Panier
